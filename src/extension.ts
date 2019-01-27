0efe7482f62759c5b446097535187046177119b5
A2K <a2k0001@gmail.com>
2019-01-27 06:11:56 +0200

    version 0.3.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 import * as cp from 'child_process';
 import * as vscode from 'vscode';
@@ -8,60 +8,59 @@ import * as path from 'path';
 
 
 export interface ITask<T> {
-	(): T;
+    (): T;
 }
 
 export class Throttler<T> {
 
-	private activePromise: Promise<T> | null;
-	private queuedPromise: Promise<T> | null;
-	private queuedPromiseFactory: ITask<Promise<T>> | null;
+    private activePromise: Promise<T> | null;
+    private queuedPromise: Promise<T> | null;
+    private queuedPromiseFactory: ITask<Promise<T>> | null;
 
-	constructor() {
+    constructor() {
         this.activePromise = null;
-		this.queuedPromise = null;
-		this.queuedPromiseFactory = null;
-	}
+        this.queuedPromise = null;
+        this.queuedPromiseFactory = null;
+    }
 
-	public queue(promiseFactory: ITask<Promise<T>>): Promise<T> {
-		if (this.activePromise) {
-			this.queuedPromiseFactory = promiseFactory;
+    public queue(promiseFactory: ITask<Promise<T>>): Promise<T> {
+        if (this.activePromise) {
+            this.queuedPromiseFactory = promiseFactory;
 
-			if (!this.queuedPromise) {
-				var onComplete = () => {
+            if (!this.queuedPromise) {
+                var onComplete = () => {
                     this.queuedPromise = null;
-                    
+
                     if (!this.queuedPromiseFactory) {
                         return new Promise<T>((resolve, reject) => {
                             resolve();
                         });
-                    } 
+                    }
 
-					var result = this.queue(this.queuedPromiseFactory);
-					this.queuedPromiseFactory = null;
+                    var result = this.queue(this.queuedPromiseFactory);
+                    this.queuedPromiseFactory = null;
 
-					return result;
-				};
+                    return result;
+                };
 
-				this.queuedPromise = new Promise<T>((resolve, reject) => {
+                this.queuedPromise = new Promise<T>((resolve, reject) => {
                     if (this.activePromise) {
                         this.activePromise.then(onComplete, onComplete).then(resolve);
                     }
-				});
-			}
+                });
+            }
 
-			return new Promise<T>((resolve, reject) => {
-				if (this.queuedPromise) {
+            return new Promise<T>((resolve, reject) => {
+                if (this.queuedPromise) {
                     this.queuedPromise.then(resolve, reject);
                 }
-			});
-		}
+            });
+        }
 
-		this.activePromise = promiseFactory();
+        this.activePromise = promiseFactory();
 
-		return new Promise<T>((resolve, reject) => {
-            if (this.activePromise)
-            {
+        return new Promise<T>((resolve, reject) => {
+            if (this.activePromise) {
                 this.activePromise.then((result: T) => {
                     this.activePromise = null;
                     resolve(result);
@@ -70,77 +69,77 @@ export class Throttler<T> {
                     reject(err);
                 });
             }
-		});
-	}
+        });
+    }
 }
 
 export class Delayer<T> {
 
-	public defaultDelay: number;
-	private timeout: NodeJS.Timer | null;
-	private completionPromise: Promise<T>| null;
-	private onResolve: ((value: T | Thenable<T> | undefined) => void) | null;
-	private task: ITask<T> | null;
+    public defaultDelay: number;
+    private timeout: NodeJS.Timer | null;
+    private completionPromise: Promise<T> | null;
+    private onResolve: ((value: T | Thenable<T> | undefined) => void) | null;
+    private task: ITask<T> | null;
 
-	constructor(defaultDelay: number) {
-		this.defaultDelay = defaultDelay;
-		this.timeout = null;
-		this.completionPromise = null;
-		this.onResolve = null;
-		this.task = null;
-	}
+    constructor(defaultDelay: number) {
+        this.defaultDelay = defaultDelay;
+        this.timeout = null;
+        this.completionPromise = null;
+        this.onResolve = null;
+        this.task = null;
+    }
 
-	public trigger(task: ITask<T>, delay: number = this.defaultDelay): Promise<T> {
-		this.task = task;
-		this.cancelTimeout();
+    public trigger(task: ITask<T>, delay: number = this.defaultDelay): Promise<T> {
+        this.task = task;
+        this.cancelTimeout();
 
-		if (!this.completionPromise) {
-			this.completionPromise = new Promise<T>((resolve, reject) => {
-				this.onResolve = resolve;
-			}).then(() => {
-				this.completionPromise = null;
-				this.onResolve = null;
+        if (!this.completionPromise) {
+            this.completionPromise = new Promise<T>((resolve, reject) => {
+                this.onResolve = resolve;
+            }).then(() => {
+                this.completionPromise = null;
+                this.onResolve = null;
 
                 if (this.task === null) {
                     return new Promise<T>((resolve, reject) => {
                         resolve();
                     });
                 }
-				var result = this.task();
-				this.task = null;
+                var result = this.task();
+                this.task = null;
 
-				return result;
-			});
-		}
+                return result;
+            });
+        }
 
-		this.timeout = setTimeout(() => {
+        this.timeout = setTimeout(() => {
             this.timeout = null;
             if (this.onResolve) {
                 this.onResolve(undefined);
             }
-		}, delay);
+        }, delay);
 
-		return this.completionPromise;
-	}
+        return this.completionPromise;
+    }
 
-	public isTriggered(): boolean {
-		return this.timeout !== null;
-	}
+    public isTriggered(): boolean {
+        return this.timeout !== null;
+    }
 
-	public cancel(): void {
-		this.cancelTimeout();
+    public cancel(): void {
+        this.cancelTimeout();
 
-		if (this.completionPromise) {
-			this.completionPromise = null;
-		}
-	}
+        if (this.completionPromise) {
+            this.completionPromise = null;
+        }
+    }
 
-	private cancelTimeout(): void {
-		if (this.timeout !== null) {
-			clearTimeout(this.timeout);
-			this.timeout = null;
-		}
-	}
+    private cancelTimeout(): void {
+        if (this.timeout !== null) {
+            clearTimeout(this.timeout);
+            this.timeout = null;
+        }
+    }
 }
 
 /**
@@ -152,17 +151,17 @@ export class Delayer<T> {
  */
 export class ThrottledDelayer<T> extends Delayer<Promise<T>> {
 
-	private throttler: Throttler<T>;
+    private throttler: Throttler<T>;
 
-	constructor(defaultDelay: number) {
-		super(defaultDelay);
+    constructor(defaultDelay: number) {
+        super(defaultDelay);
 
-		this.throttler = new Throttler();
-	}
+        this.throttler = new Throttler();
+    }
 
-	public trigger(promiseFactory: ITask<Promise<T>>, delay?: number): Promise<Promise<T>> {
-		return super.trigger(() => this.throttler.queue(promiseFactory), delay);
-	}
+    public trigger(promiseFactory: ITask<Promise<T>>, delay?: number): Promise<Promise<T>> {
+        return super.trigger(() => this.throttler.queue(promiseFactory), delay);
+    }
 }
 
 enum RunTrigger {
@@ -178,7 +177,7 @@ namespace RunTrigger {
         onType: 'onType',
         never: 'never'
     };
-    export let from = function(value: string): RunTrigger {
+    export let from = function (value: string): RunTrigger {
         if (value === 'onSave') {
             return RunTrigger.onSave;
         } else if (value === 'onType') {
@@ -200,31 +199,28 @@ class CompilerOutputParser {
     private lineOffset: number = 0;
 
     constructor(filename_: string, document_: vscode.TextDocument) {
-        this.filename = filename_;    
+        this.filename = filename_;
         this.document = document_;
     }
 
-    public write(buffer: Buffer)
-    {
+    public write(buffer: Buffer) {
         this.data += buffer.toString();
         var newlineIndex = -1;
-        while((newlineIndex = this.data.indexOf('\n')) > -1)
-        {
+        while ((newlineIndex = this.data.indexOf('\n')) > -1) {
             this.processLine(this.data.substr(0, newlineIndex));
             this.data = this.data.substr(newlineIndex + 1);
         }
     }
 
     private processLine(line: string) {
-        
+
         if (!line.startsWith(this.filename)) {
             return;
         }
-        
+
         let parts: string[] = line.substring(this.filename.length + 1).split(":");
 
-        if (parts.length > 3)
-        {
+        if (parts.length > 3) {
             let line: number = Math.max(0, parseInt(parts.shift() || '0') - 1 + this.lineOffset);
             let column: number = Math.max(0, parseInt(parts.shift() || '0') - 1);
 
@@ -236,16 +232,13 @@ class CompilerOutputParser {
 
             let severity: vscode.DiagnosticSeverity = vscode.DiagnosticSeverity.Information;
 
-            if (0 === severityStr.localeCompare('warning')) 
-            {
+            if (0 === severityStr.localeCompare('warning')) {
                 severity = vscode.DiagnosticSeverity.Warning;
             }
-            else if (0 === severityStr.localeCompare('error'))
-            {
+            else if (0 === severityStr.localeCompare('error')) {
                 severity = vscode.DiagnosticSeverity.Error;
             }
-            else if (0 === severityStr.localeCompare('note'))
-            {
+            else if (0 === severityStr.localeCompare('note')) {
                 severity = vscode.DiagnosticSeverity.Hint;
             }
 
@@ -259,8 +252,7 @@ class CompilerOutputParser {
         return this.diagnostics;
     }
 
-    public setLineOffset(value: number): void
-    {
+    public setLineOffset(value: number): void {
         this.lineOffset = value;
     }
 }
@@ -268,8 +260,8 @@ class CompilerOutputParser {
 export default class HLSLLintingProvider implements vscode.Disposable {
 
     private diagnosticCollection: vscode.DiagnosticCollection;
-    
-    private executable: string = "D:\\Desktop\\DXC\\bin\\dxc.exe"; // TODO
+
+    private executable: string = "dxc";
 
     private executableNotFound: boolean = false;
 
@@ -281,12 +273,12 @@ export default class HLSLLintingProvider implements vscode.Disposable {
 
     private delayers: { [key: string]: ThrottledDelayer<void> } = {};
 
-    private documentListener: vscode.Disposable = { dispose: () => {} };
+    private documentListener: vscode.Disposable = { dispose: () => { } };
 
     constructor() {
         this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
-	}
-	
+    }
+
     public activate(subscriptions: vscode.Disposable[]): void {
         subscriptions.push(this);
         vscode.workspace.onDidChangeConfiguration(this.loadConfiguration, this, subscriptions);
@@ -297,10 +289,10 @@ export default class HLSLLintingProvider implements vscode.Disposable {
             this.diagnosticCollection.delete(textDocument.uri);
         }, null, subscriptions);
 
-		vscode.workspace.textDocuments.forEach(this.triggerLint, this);
+        vscode.workspace.textDocuments.forEach(this.triggerLint, this);
     }
-	
-	private triggerLint(textDocument: vscode.TextDocument): void {
+
+    private triggerLint(textDocument: vscode.TextDocument): void {
         if (textDocument.languageId !== 'hlsl' || this.executableNotFound) {
             return;
         }
@@ -317,22 +309,21 @@ export default class HLSLLintingProvider implements vscode.Disposable {
             delayer = new ThrottledDelayer<void>(this.trigger === RunTrigger.onType ? 250 : 0);
             this.delayers[key] = delayer;
         }
-        delayer.trigger(() => this.doLint(textDocument) );
-	}
+        delayer.trigger(() => this.doLint(textDocument));
+    }
 
-	private doLint(textDocument: vscode.TextDocument): Promise<void> {
+    private doLint(textDocument: vscode.TextDocument): Promise<void> {
 
         return new Promise<void>((resolve, reject) => {
 
             let filename = tempfile('.hlsl');
 
             let cleanup = ((filename: string) => {
-                fs.unlink(filename, (err: Error) => {});
+                fs.unlink(filename, (err: Error) => { });
             }).bind(this, filename);
-            
+
             let text = textDocument.getText();
-            if (textDocument.fileName.endsWith(".ush"))
-            {
+            if (textDocument.fileName.endsWith(".ush")) {
                 text = text.replace(/#pragma\s+once[^\n]*\n/g, '//#pragma once\n');
             }
 
@@ -341,21 +332,21 @@ export default class HLSLLintingProvider implements vscode.Disposable {
             let decoder = new CompilerOutputParser(filename, textDocument);
 
             let options = vscode.workspace.rootPath ? { cwd: vscode.workspace.rootPath } : undefined;
-            
+
             let args: string[] = Array.from(this.defaultArgs);
             /* [
                 '-Od', // disable optimizations
                 '-Ges' // enable strict mode
             ];
-            */                
+            */
 
             const re = /\/\/\s*INPUTS(?:\((\w+)\))?:\s*([^\n]+)\s*\n/g;
             //const re = /\/\/\s*INPUTS:\s*([^\n]+)\s*\n/g;
 
             var symbols: string[] = [];
 
-            var predefined: { [key:string]: string} = {};
-            
+            var predefined: { [key: string]: string } = {};
+
             var m;
             while (m = re.exec(text)) {
 
@@ -365,17 +356,16 @@ export default class HLSLLintingProvider implements vscode.Disposable {
 
                 var typeName: string = 'float';
 
-                if (m.length === 3 && typeof(m[1]) !== 'undefined') {
+                if (m.length === 3 && typeof (m[1]) !== 'undefined') {
                     typeName = m[1];
                 }
 
-                function makeTypeContructor(typeName: string)
-                {
-                    if (typeof(typeName) === 'undefined') {
+                function makeTypeContructor(typeName: string) {
+                    if (typeof (typeName) === 'undefined') {
                         return '0';
                     }
-                    
-                    const Constructors: { [key:string]: string } = {
+
+                    const Constructors: { [key: string]: string } = {
                         'float': '0.0',
                         'float2': 'float2(0, 0)',
                         'float3': 'float3(0, 0, 0)',
@@ -392,19 +382,17 @@ export default class HLSLLintingProvider implements vscode.Disposable {
 
                     return null;
                 }
-                
+
                 ((m.length === 2) ? m[1] : m[2]).split(/\s*,\s*/).forEach((symbol: string) => {
                     symbol = symbol.trim();
                     var existingSymbol = symbols.find((s) => 0 === s.localeCompare(symbol));
-                    if (typeof(existingSymbol) === 'undefined' || null === existingSymbol) {
+                    if (typeof (existingSymbol) === 'undefined' || null === existingSymbol) {
                         symbols.push(symbol);
                         let typeConstructor = makeTypeContructor(typeName);
-                        if (typeConstructor === null)
-                        {
+                        if (typeConstructor === null) {
                             predefined[symbol] = typeName;
                         }
-                        else
-                        {
+                        else {
                             args.push('-D');
                             args.push(symbol + '=' + typeConstructor);
                         }
@@ -419,16 +407,15 @@ export default class HLSLLintingProvider implements vscode.Disposable {
 
             args.push('-I');
             args.push(path.dirname(textDocument.fileName));
-            
-            for(var includeDir in this.includeDirs)
-            {
+
+            for (var includeDir in this.includeDirs) {
                 args.push("-I");
                 args.push(includeDir);
             }
-            
+
             args.push(filename);
 
-            var addedLines = 0;            
+            var addedLines = 0;
             let prefix: string = "";
             Object.keys(predefined).forEach((key) => {
                 prefix += predefined[key] + ' ' + key + ';\n';
@@ -438,16 +425,16 @@ export default class HLSLLintingProvider implements vscode.Disposable {
             decoder.setLineOffset(-addedLines);
 
             text = prefix + text;
-            
+
             fs.writeFile(filename, Buffer.from(text, 'utf8'), ((err: Error) => {
 
-                if(err) {
+                if (err) {
                     console.log('error:', err);
                     cleanup();
                     return;
                 }
 
-                console.log(`Starting "${executable} ${args.join(' ')}"`);
+                //console.log(`Starting "${executable} ${args.join(' ')}"`);
 
                 let childProcess = cp.spawn(executable, args, options);
                 childProcess.on('error', (error: Error) => {
@@ -475,12 +462,10 @@ export default class HLSLLintingProvider implements vscode.Disposable {
                     });
                     childProcess.stderr.on('end', () => {
                         let diagnostics: vscode.Diagnostic[] = decoder.getDiagnostics();
-                        if (diagnostics.length)
-                        {
+                        if (diagnostics.length) {
                             this.diagnosticCollection.set(textDocument.uri, diagnostics);
                         }
-                        else
-                        {
+                        else {
                             this.diagnosticCollection.delete(textDocument.uri);
                         }
                         cleanup();
@@ -489,14 +474,14 @@ export default class HLSLLintingProvider implements vscode.Disposable {
                 } else {
                     cleanup();
                     resolve();
-                }                    
-            }).bind(this)); 
-        });        
+                }
+            }).bind(this));
+        });
     }
 
     private loadConfiguration(): void {
 
-        let section = vscode.workspace.getConfiguration('hlsl');        
+        let section = vscode.workspace.getConfiguration('hlsl');
 
         if (section) {
             this.executable = section.get<string>('linter.executablePath', "D:\\Desktop\\DXC\\bin\\dxc.exe");
@@ -520,17 +505,14 @@ export default class HLSLLintingProvider implements vscode.Disposable {
 
     }
 
-    public dispose()
-    {
+    public dispose() {
 
     }
 }
 
 export function activate(context: vscode.ExtensionContext): void {
-	let linter = new HLSLLintingProvider();	
-	linter.activate(context.subscriptions);
+    let linter = new HLSLLintingProvider();
+    linter.activate(context.subscriptions);
 }
 
-export function deactivate() {}
-
-
+export function deactivate() { }
